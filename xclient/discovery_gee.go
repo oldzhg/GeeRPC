2f086dedab397d059a1ff5b9d521296a86e85efc
oldzhg <zcy000321@gmail.com>
2022-08-01 15:14:21 +0800

    init

---
+++ xclient/discovery_gee.go
@@ -0,0 +1,76 @@
+package xclient
+
+import (
+	"log"
+	"net/http"
+	"strings"
+	"time"
+)
+
+type GeeRegistryDiscovery struct {
+	*MultiServersDiscovery
+	registry   string        // 注册中心的地址
+	timeout    time.Duration // 服务列表的过期时间
+	lastUpdate time.Time     //代表最后从注册中心更新服务列表的时间，默认 10s 过期
+}
+
+func NewGeeRegistryDiscovery(registry string, timeout time.Duration) *GeeRegistryDiscovery {
+	if timeout == 0 {
+		timeout = defaultUpdateTimeout
+
+	}
+	d := &GeeRegistryDiscovery{
+		MultiServersDiscovery: NewMultiServersDiscovery(make([]string, 0)),
+		registry:              registry,
+		timeout:               timeout,
+	}
+	return d
+}
+
+const defaultUpdateTimeout = time.Second * 10
+
+func (d *GeeRegistryDiscovery) Update(servers []string) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	d.servers = servers
+	d.lastUpdate = time.Now()
+	return nil
+}
+
+func (d *GeeRegistryDiscovery) Refresh() error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	// 没过期
+	if d.lastUpdate.Add(d.timeout).After(time.Now()) {
+		return nil
+	}
+	log.Println("rpc registry: refresh servers from registry", d.registry)
+	resp, err := http.Get(d.registry)
+	if err != nil {
+		log.Println("rpc registry refresh err:", err)
+		return err
+	}
+	servers := strings.Split(resp.Header.Get("X-Geerpc-Servers"), ",")
+	d.servers = make([]string, 0, len(servers))
+	for _, server := range servers {
+		if strings.TrimSpace(server) != "" {
+			d.servers = append(d.servers, strings.TrimSpace(server))
+		}
+	}
+	d.lastUpdate = time.Now()
+	return nil
+}
+
+func (d *GeeRegistryDiscovery) Get(mode SelectMode) (string, error) {
+	if err := d.Refresh(); err != nil {
+		return "", err
+	}
+	return d.MultiServersDiscovery.Get(mode)
+}
+
+func (d *GeeRegistryDiscovery) GetAll() ([]string, error) {
+	if err := d.Refresh(); err != nil {
+		return nil, err
+	}
+	return d.MultiServersDiscovery.GetAll()
+}
